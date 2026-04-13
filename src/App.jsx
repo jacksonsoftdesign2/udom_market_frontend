5eb1985c60aca9e3f040179ae1db9972a07e5608
Jackson <jacksonduwanghe2@gmail.com>
2026-04-13 15:48:50 +0300

    i have to add and modify app and login respectively

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+
 import Home from "./pages/Home";
 import RegisterTrader from "./pages/RegisterTrader";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -10,6 +12,10 @@ function App() {
 
         {/* HOME */}
         <Route path="/" element={<Home />} />
+
+
+        {/* LOGIN */}
+        <Route path="/login" element={<Login />} />
 
         {/* REGISTER */}
         <Route path="/register-trader" element={<RegisterTrader />} />
