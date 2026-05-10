e2a40c052720768b72acf344eea6a6b1facc18d7
Jackson <jacksonduwanghe2@gmail.com>
2026-05-10 04:31:46 +0300

    sp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import gbFlag from "../assets/gb.png";
 import logo from "../assets/upmarket_logo.png";
 import translations from "../translations";
 import { FaUser, FaInfoCircle, FaTruckMoving, FaHome, FaChevronRight } from "react-icons/fa";
-import Breadcrumb from "./Breadcrumb";
+
 export default function Header({ cartCount, stickySearch }) {
   const navigate = useNavigate();
   const location = useLocation();
@@ -338,7 +338,7 @@ useEffect(() => {
         </div>
       </div>
 
-       <Breadcrumb />
+
 
       {/* 🔽 DROPDOWN MENU - MOBILE OPTIMIZED */}
       {menuOpen && (
